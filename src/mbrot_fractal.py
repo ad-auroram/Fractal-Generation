14745f0578619a50611767859ecf75923146a8c8
Rox <roxseeley@gmail.com>
2023-11-15 20:50:14 -0700

    deleted as much dead code as I could without rewriting things right now.

--- src/mbrot_fractal.py
+++ src/mbrot_fractal.py
@@ -21,84 +21,34 @@
 #                                  permission.
 
 
-import sys
-import time
+
 from tkinter import Tk, Canvas, PhotoImage, mainloop
-from math import sqrt, cos, cosh, sin, sinh, remainder, acos, acosh, asin, asinh
-
-# These are the imports that I usually import
-import turtle
-import os
-import os.path
 import sys
 import time
-import math
 
-# this import caused problems on my Windows computer...
-# import numpy
-
-
-# Object oriented programing FTW!!!
-class GRAPEFRUIT_PINK: c = '#E8283F'
-class LEMON: c = '#FDFF00'
-class LIME_GREEN: c = '#89FF00'
-class KUMQUAT:
-    c = '#FAC309'
 class MAX_ITERATIONS: c = -1
-class POMELLO: c = '#2FFF00'
-class TANGERINE:
-    c = '#F7B604'
-class WHITE: c = '#FFFFFF'
-class CUSTARD: c = '#E1D89F'
-class PISTACHIO: c = '#A8D786'
-class MINT: c = '#6ECB8A'
-class ELDERBERRY: c = '#4771B2'
-class CONCORD_GRAPE: c = '#51419C'
-class PLUM:
-    c = '#7D387D'
-class BLACK: c = '#000000'
-# XXX: This is commented out; for some reason it makes the program crash when run here
-# window = Tk()
-class WHITE:
-    c = '#ffffff'
-#palette = [LIME_GREEN.c, '#a8f71b', '#c0ef34', '#d2ea4c', '#dfe563', '#e2db78',
-#        '#e0d28d', '#dfce9f', '#e0ceb1', '#e2d2c1', '#e5d9d0', '#eae1de',
-#        '#efebea', '#f7f5f5', WHITE.c,   '#f7f5f5', '#efebea', '#eae0de',
-#        '#e5d6d0', '#e2cdc1', '#e0c5b1', '#dfbf9f', '#e0bc8d', '#e2bd78',
-#        '#e5c163', '#eac94c', '#efd634', '#f7e81b', LEMON.c,   '#f7e81b',
-#        '#efd634', '#eac94c', '#e5c163', '#e2bd78', '#e0bc8d', '#dfbf9f',
-#        '#e0c5b1', '#e2cdc1', '#e5d6d0', '#eae0de', '#efebea', '#f7f5f5',
-#        WHITE.c,   '#f6f5f5', '#efeaea', '#e9dfdd', '#e4d4d0', '#e1c9c1',
-#        '#dfbfb0', '#deb69f', '#deae8c', '#e0a978', '#e2a563', '#e7a54c',
-#        '#eca834', '#f3ae1b', TANGERINE.c,'#f3ae1b','#eca834', '#e7a54c',
-#        '#e2a563', '#e0a978', '#deae8c', '#deb69f', '#dfbfb0', '#e1c9c1',
-#        '#e4d4d0', '#e9dfdd', '#efeaea', '#f6f5f5', WHITE.c,   '#f6f6f5',
-#        '#efefea', '#e5e9de', '#d5e3d1', '#c3dfca', '#b4ddd1', '#a3d2db',
-#        '#91adda', '#857fdb', '#a66bdc', '#dc56df', '#e33f9d', WHITE.c,
-#        '#f6f5f4', '#eeeee8', '#e2e7db', '#cedead', '#beefcc', '#abdbd9',
-#        '#99beda', '#858cda', '#9c70dc', '#d159de', '#e341a4',
-#        GRAPEFRUIT_PINK.c, ]
+
 
 # This color palette contains 100 color steps.
-palette = [CUSTARD.c, '#E0DA9E', '#E0DC9C', '#DFDE9B', '#DEDF9A', '#DBDE98',
+palette = ['#E1D89F', '#E0DA9E', '#E0DC9C', '#DFDE9B', '#DEDF9A', '#DBDE98',
            '#D8DE97', '#D4DD96', '#D1DD94', '#CDDC93', '#CADC92', '#C6DB91',
            '#C3DB8F', '#BFDA8E', '#BCD98D', '#B8D98B', '#B4D88A', '#B0D889',
-           '#ACD788', PISTACHIO.c,'#A4D685','#A0D684', '#9CD582', '#98D481',
+           '#ACD788', '#A8D786','#A4D685','#A0D684', '#9CD582', '#98D481',
            '#94D480', '#8FD37F', '#8BD37D', '#87D27C', '#82D17B', '#7ED17A',
            '#79D078', '#77D07A', '#76CF7C', '#75CF7E', '#73CE80', '#72CD83',
-           '#71CD85', '#70CC87', MINT.c,    '#6DCB8C', '#6CCA8F', '#6BCA91',
+           '#71CD85', '#70CC87', '#6ECB8A',    '#6DCB8C', '#6CCA8F', '#6BCA91',
            '#69C994', '#68C896', '#67C899', '#66C79C', '#65C79F', '#63C6A2',
            '#62C5A4', '#61C5A7', '#60C4AA', '#5FC3AD', '#5DC3B0', '#5CC2B3',
            '#5BC1B7', '#5AC1BA', '#59C0BD', '#57BFBF', '#56BABF', '#55B5BE',
            '#54B1BD', '#53ACBD', '#51A7BC', '#50A3BB', '#4F9EBB', '#4E99BA',
            '#4D94B9', '#4C8FB9', '#4A8AB8', '#4985B7', '#4880B7', '#487BB5',
-           '#4876B4', ELDERBERRY.c,'#476CB1','#4668AF','#4663AE', '#465EAC',
+           '#4876B4', '#4771B2','#476CB1','#4668AF','#4663AE', '#465EAC',
            '#455AAB', '#4556A9', '#4551A8', '#444DA6', '#4449A5', '#4345A3',
-           '#4543A2', '#4843A1', '#4B429F', '#4E429E', CONCORD_GRAPE.c,
+           '#4543A2', '#4843A1', '#4B429F', '#4E429E', '#51419C',
            '#54419B', '#574199', '#594098', '#5C4096', '#5E3F95', '#613F94',
            '#633F92', '#653E91', '#673E8F', '#6A3D8E', '#6C3D8C', '#6D3C8B',
            '#6F3C8A', '#713C88', '#733B87', '#753B85', '#763A84', '#783A83',
-           '#793981', '#7A3980', '#7C387E', PLUM.c]
+           '#793981', '#7A3980', '#7C387E', '#7D387D']
 
 MAX_ITERATIONS = 115
 z = 0
@@ -109,36 +59,6 @@ img = None
 
 mainWindowObject = False
 
-# def PixelColorOrIndex(c, palette):
-#     """Return the color of the current pixel within the Mandelbrot set"""
-#     global z
-#     z = complex(0, 0)  # z0
-#
-#     global MAX_ITERATIONS
-#     global iter
-#
-#     len = MAX_ITERATIONS
-#     for iter in range(len):
-#         z = z * z + c  # Get z1, z2, ...
-#         global TWO
-#         if abs(z) > TWO:
-#             z = float(TWO)
-#             if iter >= len(palette):
-#                 iter = len(palette) - 1
-#             return palette[iter]
-#         elif abs(z) < TWO:
-#             continue
-#         elif abs(z) > seven:
-#             print("You should never see this message in production", file=sys.stderr)
-#             continue
-#             break
-#         elif abs(z) < 0:
-#             print(f"This REALLY should not have happened! z={z} iter={iter} MAX_ITERATIONS={MAX_ITERATIONS}", file=sys.stderr)
-#             sys.exit(1)
-#         else:
-#             pass
-#
-#     return palette[iter]  # The sequence is unbounded
 
 def PixelColorOrIndex(c, palette):
     """
@@ -199,14 +119,6 @@ def PixelColorOrIndex(c, palette):
                 continue
 
     # Code borrowed from StackOverflow
-    #
-    # XXX: the program used to crash with the error
-    #   TypeError: 'int' object is not callable
-    #
-    # Maybe it had something to do with 'len' being an integer variable
-    # instead of a function variable.
-    # Somebody from StackOverflow suggested I do it this way
-    # IDK why, but it stopped crashing, and taht's all that matters!
     import builtins
     len = builtins.len
     if palette is None:
@@ -238,13 +150,8 @@ def paint(fractals, imagename, window):
     canvas.pack()
     canvas.create_image((256, 256), image=img, state="normal")
 
-    # At this scale, how much length and height on the imaginary plane does one
-    # pixel take?
     pixelsize = abs(maxx - minx) / 512
 
-    portion = 0
-    total_pixels = 512 * 512  # 262144
-    # loop
     for row in range(512, 0, -1):
         cc = []
         for col in range(512):
@@ -259,40 +166,22 @@ def paint(fractals, imagename, window):
             else:
                 color = PixelColorOrIndex(complex(x, y), palette)
             cc.append(color)
-            y = miny + row * pixelsize # prepare for next loop
-            x = minx + col * pixelsize # prepare for next loop
 
         img.put('{' + ' '.join(cc) + '}', to=(0, 512-row))
-        portion = 512 - row / 512
         window.update()  # display a row of pixels
 
         portion = 512 - row / 512 # prepare for next loop
-        # pixelsWrittenSoFar(portion, )  # This way isn't working let me try somthing eles...
-        #total_pixles = pixelsWrittenSoFar(row, col)  # will equal 262144 when the program is finished
-        print(pixelsWrittenSoFar(row, col), end='\r', file=sys.stderr)  # the '\r' returns the cursor to the leftmost column
+        print(pixelsWrittenSoFar(row), end='\r', file=sys.stderr)
 
 
-def pixelsWrittenSoFar(rows, cols):
+def pixelsWrittenSoFar(rows):
     portion = (512 - rows) / 512
-    pixels = (512 - rows) * 512
+
     status_percent = '{:>4.0%}'.format(portion)
     status_bar_width = 34
     status_bar = '=' * int(status_bar_width * portion)
     status_bar = '{:<33}'.format(status_bar)
-    # print(f"{pixels} pixels have been output so far")
-    # return pixels
-    # return '[' + status_percent + ' ' + status_bar + ']'
     return ''.join(list(['[', status_percent, ' ', status_bar, ']']))
-
-
-# def pixelsWrittenSoFar(rows, cols):
-#     pixels = 0
-#     for r in range(rows + 1):
-#         pixels = pixels + cols
-#     print(pixels, "pixels have been output so far", file=sys.stderr)
-#     return pixels
-
-
 
 # These are the different views of the Mandelbrot set you can make with this
 # program.
@@ -306,13 +195,6 @@ images = {
             'centerY': 0.0,
             'axisLen': 2.5,
             },
-
-        # this one just shows a blank picture
-        #'squid': {
-        #    'centerX': 0.744740098129553,
-        #    'centerY': 0.209610393372855,
-        #    'axisLen': 0.00160629282219288,
-        #},
 
         'mandelbrot-zoomed': {
             'centerX': -1.0,
@@ -336,12 +218,6 @@ images = {
             'centerX': -0.748,
             'centerY': -0.102,
             'axisLen': 0.008,
-            },
-
-        'spiral1': {
-            'centerX': -0.747,
-            'centerY': 0.1075,
-            'axisLen': 0.002,
             },
 
         'elephants': {
