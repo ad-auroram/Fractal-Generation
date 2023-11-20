b4195888ac5dac5c4db5ec51f1984254da0c08b4
Erik on Mariner <erik.falor@usu.edu>
2023-11-20 14:02:48 -0700

    Erik's solution to Assignment 4.0

---
+++ erik-src/mandelbrot.py
@@ -0,0 +1,30 @@
+#              Copyright © 2023 DuckieCorp. All Rights Reserved.
+#
+#                       __      Redistribution and use of this code, with or
+#                     /` ,\__   without modification, are permitted provided
+#                    |    ).-'  that the following conditions are met:
+#                   / .--'
+#                  / /          0. Redistributions of this code must retain
+#    ,      _.==''`  \             the above copyright notice, this list of
+#  .'(  _.='         |             conditions and the following disclaimer.
+# {   ``  _.='       |          1. The name of the author may not be used to
+#  {    \`     ;    /              endorse or promote products derived from
+#   `.   `'=..'  .='               this software without specific prior written
+#     `=._    .='                  permission.
+#  jgs  '-`\\`__                2. Neither the name of the University nor the
+#           `-._{                  names of its contributors may be used to
+#                                  endorse or promote products derived from
+#                                  this software without specific prior written
+#                                  permission.
+
+def count(c, end):
+    """
+    Return the iteration count for a point on the complex plane `c`
+    to guess whether it is in the Mandelbrot set (bounded by `end`)
+    """
+    z = complex(0, 0)  # z0
+    for i in range(end):
+        z = z * z + c  # Get z1, z2, ...
+        if abs(z) > 2.0:
+            return i
+    return end
